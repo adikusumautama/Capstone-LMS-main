28f60ef8518937b3ceeddfcbff2cfad0caa019c4
Gittt13 <muhammad.1288@students.amikom.ac.id>
2023-12-14 20:26:00 +0700

    animasi bg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+      darkMode: "class",
+    }
+    </script>
     <style>
+        /* style form */
         * {
             font-family: 'Poppins' !important;
         }
-        body {
+
+        /* body {
             background-repeat:no-repeat;
-        }
+        } */
         .login-box {
             position: absolute;
             top: 50%;
@@ -128,13 +137,101 @@
             background: linear-gradient(90deg, transparent, red);
             animation: btn-anim1 2s linear infinite;
         }
+
+        /* end style form */
+
+        /* Style Bg Theme */
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+        body {
+            font-family: Inter, sans-serif;
+        }
+
+        @keyframes jumbo {
+            from {
+                background-position: 50% 50%, 50% 50%;
+            }
+
+            to {
+                background-position: 350% 50%, 350% 50%;
+            }
+        }
+
+        .jumbo {
+            --stripes: repeating-linear-gradient(100deg,
+                    #fff 0%,
+                    #fff 7%,
+                    transparent 10%,
+                    transparent 12%,
+                    #fff 16%);
+            --stripesDark: repeating-linear-gradient(100deg,
+                    #000 0%,
+                    #000 7%,
+                    transparent 10%,
+                    transparent 12%,
+                    #000 16%);
+            --rainbow: repeating-linear-gradient(100deg,
+                    #60a5fa 10%,
+                    #e879f9 15%,
+                    #60a5fa 20%,
+                    #5eead4 25%,
+                    #60a5fa 30%);
+            background-image: var(--stripes), var(--rainbow);
+            background-size: 300%, 200%;
+            background-position: 50% 50%, 50% 50%;
+
+            filter: blur(10px) invert(100%);
+
+            mask-image: radial-gradient(ellipse at 100% 0%, black 40%, transparent 70%);
+
+            pointer-events: none;
+        }
+
+        .jumbo::after {
+            content: "";
+            position: absolute;
+            inset: 0;
+            background-image: var(--stripes), var(--rainbow);
+            background-size: 200%, 100%;
+            animation: jumbo 60s linear infinite;
+            background-attachment: fixed;
+            mix-blend-mode: difference;
+        }
+
+        .dark .jumbo {
+            background-image: var(--stripesDark), var(--rainbow);
+            filter: blur(10px) opacity(50%) saturate(200%);
+        }
+
+        .dark .jumbo::after {
+            background-image: var(--stripesDark), var(--rainbow);
+        }
+
+        /* End Style Bg Theme */
     </style>
 </head>
 
-<body
-    style="background-image: url('{{ asset('frontend/img/ptn-2.jpg') }}'); background-size: cover; position: center; background-size: 100%;">
+<body>
+
+<div class="relative flex flex-col h-[100vh] items-center bg-white dark:bg-black transition-bg">
+    <div class="absolute inset-0 overflow-hidden">
+        <div class="jumbo absolute -inset-[10px] opacity-50"></div>
+    </div>
+    <!-- <h1
+        class="relative flex items-center text-5xl font-bold text-gray-800 dark:text-white dark:opacity-80 transition-colors">
+        charm
+        <span class="ml-1 rounded-xl bg-current p-2 text-[0.7em] leading-none">
+            <span class="text-white dark:text-black">UI</span>
+        </span>
+    </h1> -->
+    <div class="mt-40 log-in">
+        <button onclick="toggleTheme()"
+            class="px-3 py-1 border border-stone-200 rounded-full drop-shadow-sm text-sm text-stone-800 dark:text-white bg-white/40 dark:bg-black/40 backdrop-blur-lg hover:border-stone-300 transition-colors dark:border-stone-500 dark:hover:border-stone-400">
+            Click Me</button>
+    </div>
+</div>
     <div class="login-box">
-        <h1 style="color: white;">Login</h1>
+        <h1 style="color: #427D9D;">Login</h1>
 
         <form id="loginForm" method="POST" action="{{ route('login') }}">
             {{ csrf_field() }}
@@ -152,11 +249,14 @@
             </div>
             <center>
                 <div type="submit" class="container" style="gap: 0.5rem !important;">
-                    <button type="submit" class="log-in" style="background-color: rgba(24, 20, 20, 0.987); border-radius:6px; margin-right:20px;">
+                    <button type="submit" class="log-in"
+                        style="background-color: rgba(24, 20, 20, 0.987); border-radius:6px; margin-right:20px;">
                         Login
                         <span class="span-login"></span>
                     </button>
-                    <button type="button" class="daftar" style="background-color: rgba(24, 20, 20, 0.987); border-radius:6px;" onclick="window.location.href='{{ route('register') }}'">
+                    <button type="button" class="daftar"
+                        style="background-color: rgba(24, 20, 20, 0.987); border-radius:6px;"
+                        onclick="window.location.href='{{ route('register') }}'">
                         Register
                         <span class="span-daftar"></span>
                     </button>
@@ -187,7 +287,19 @@
                 }
             });
         });
+
+        
+        function toggleTheme() {
+            if (document.body.classList.contains("dark"))
+                document.body.classList.remove("dark");
+            else
+                document.body.classList.add("dark");
+        }
     </script>
+    
+
+
+
 </body>
 
 </html>
